01db642e9ca62209b0a6a06930801d5ec92a0667
mugithi <isackaranja@gmail.com>
2017-12-08 23:17:17 -0600

    added script to check status of services during install

---
+++ install-helm/waitForStatusCeph.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import  subprocess
+import time
+timeout=60
+DEPLOY_STATUS="DEPLOY_FAILED"
+value = "0/1"
+for i in range(timeout):
+    time.sleep(1)
+    isInstallCompleleteList = subprocess.check_output("kubectl get pods -n ceph | awk '{print $2}'", stderr=subprocess.STDOUT, shell=True).split("\n")[1:-1]
+    if value not in isInstallCompleleteList:
+        DEPLOY_STATUS="DEPLOY_COMPLETE"
+
+print DEPLOY_STATUS
